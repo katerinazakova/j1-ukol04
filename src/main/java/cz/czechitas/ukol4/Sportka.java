16905c5911444004c3769bd15fcd9c2cc2d68fbc
7ZKaty <zakova.kz@gmail.com>
2023-03-21 19:40:26 +0100

    UKOL 4 - losování dokončeno.

--- src/main/java/cz/czechitas/ukol4/Sportka.java
+++ src/main/java/cz/czechitas/ukol4/Sportka.java
@@ -8,54 +8,43 @@ import java.util.List;
  * Třída pro losování Sportky.
  */
 public class Sportka {
-  private final List<Integer> osudi = new ArrayList<>();
+    private final List<Integer> osudi = new ArrayList<>();
 
 
-  public Sportka() {
-    //TODO naplnit osudí čísly 1 až 49.
-
-    int cislo = 1;
-    while(cislo<=48){
-        cislo++;
-        System.out.println(cislo);
-      osudi.add(cislo);
-
+    public Sportka() {
+        int cislo = 0;
+        while (cislo <= 48) {
+            cislo++;
+            System.out.println(cislo);
+            osudi.add(cislo);
+        }
     }
 
-  }
+    /**
+     * Zamíchá osudí.
+     *
+     * @see Collections#shuffle(List)
+     */
+    public void zamichat() {
+        Collections.shuffle(osudi);
+    }
 
-  /**
-   * Zamíchá osudí.
-   *
-   * @see Collections#shuffle(List)
-   */
-  public void zamichat() {
-    //TODO zamíchat osudí
+    /**
+     * Vrátí seznam s 6 vylosovanými čísly.
+     *
+     * @return Seznam vylosovaných čísel.
+     * @see List#subList(int, int)
+     */
+    public List<Integer> vylosovanaCisla() {
+        return osudi.subList(0, 6);
+    }
 
- Collections.shuffle(osudi);
-
-  }
-
-  /**
-   * Vrátí seznam s 6 vylosovanými čísly.
-   *
-   * @return Seznam vylosovaných čísel.
-   *
-   * @see List#subList(int, int)
-   */
-  public List<Integer> vylosovanaCisla() {
-    //TODO Vrátit seznam prvních 6 čísel z osudí.
-
-    return osudi.subList(0,6);
-  }
-
-  /**
-   * Vrátí sedmé vylosované číslo (dodatkové číslo)
-   *
-   * @return Dodatkové číslo.
-   */
-  public Integer dodatkoveCislo() {
-    //TODO Vrátit sedmé číslo z osudí.
-    return osudi.get(6);
-  }
+    /**
+     * Vrátí sedmé vylosované číslo (dodatkové číslo)
+     *
+     * @return Dodatkové číslo.
+     */
+    public Integer dodatkoveCislo() {
+        return osudi.get(6);
+    }
 }
